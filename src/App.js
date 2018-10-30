4de7a27defaca74fa2db95ad231d70857c6a992a
vlastik <vlastikczech1@gmail.com>
2018-10-30 12:08:58 -0700

    Added apollo-boost and react-apollo libraries to set up graphql

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
 import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo ';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+const client = new ApolloClient({
+    uri: "https://vm8mjvrnv3.lp.gql.zone/graphql"
+});
+
+const App = () => (
+    <ApolloProvider client={client}>
+      <div>
+        <h2>My first Apollo React Application</h2>
       </div>
-    );
-  }
-}
+    </ApolloProvider>
+)
+
+
 
 export default App;
